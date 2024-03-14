54325d839ea0368dc4defa8e291fe09845b39fb3
AleksaSmiljic <aleksa.smiljic11@gmail.com>
2024-03-14 21:35:36 +0200

    Did some style changes and improved navbar that you can't scroll when the tab is opened

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 const Card = ({ title, description, price }) => {
   return (
-    <li className="flex justify-center items-center border-black border m-6 bg-gray-200 transform hover:scale-95 transition duration-200">
+    <li className="flex justify-center items-center border-black border m-2 bg-gray-200 transform hover:scale-95 transition duration-200">
       <div className="flex justify-between items-center">
-        <div className="p-3 md:left-2">
+        <div className="p-2 md:left-2">
           <p className="text-gray-800 font-bold text-xl font-montserrat">
             {title}
           </p>
@@ -16,7 +16,7 @@ const Card = ({ title, description, price }) => {
         <img
           src="small.png"
           alt={title}
-          className="h-32 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden right-5 m-2"
+          className="h-32 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden right-2 m-2"
         />
       </div>
     </li>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,15 +17,24 @@ const Header = () => {
     { name: "Admin", link: "/admin" },
   ];
 
+  function handleHamburgerClick() {
+    setIsOpen((oldIsOpen) => !oldIsOpen);
+    document.body.style.overflow = isOpen ? "auto" : "hidden";
+  }
+
   return (
-    <nav className="top-0 sticky z-10 p-1 border-b border-black w-full  bg-white md:flex md:items-center md:justify-between">
+    <header
+      className={`top-0 sticky z-10 p-2 border-b border-black w-full  bg-white md:flex md:items-center md:justify-between ${
+        isOpen ? "h-screen" : ""
+      }`}
+    >
       <div className="flex justify-between items-center">
-        <div className="flex item-center top-6 gap-2">
+        <div className="flex item-center top-2 gap-2">
           <Link to="/">
             <img
               src="logo.png"
               alt="caffe-academy-logo"
-              className="h-auto w-40 py-6 cursor-pointer "
+              className="h-auto w-40 py-2 cursor-pointer "
             />
           </Link>
         </div>
@@ -36,28 +45,73 @@ const Header = () => {
             </Link>
           </span>
           <span
-            onClick={() => setIsOpen((oldIsOpen) => !oldIsOpen)}
+            onClick={handleHamburgerClick}
             className="w-10 h-10 cursor-pointer md:hidden block"
           >
             {isOpen ? <XMarkIcon /> : <Bars3Icon />}
           </span>
         </span>
       </div>
-      <ul
-        className={`md:flex md:items-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-300
-        ${isOpen ? "top-[80px] opacity-100 z-50 bg-white" : ""}`}
+      <nav
+        className={`md:flex md:items-center md:w-auto ${
+          isOpen ? "" : "hidden"
+        }`}
       >
-        {Links.map((link) => (
-          <li
-            key={link.link}
-            className="hover:text-blue-400 duration-500 mx-4 my-6 md:my-0"
-          >
-            <Link to={link.link}>{link.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+        <ul className="md:flex items-center justify-between ">
+          {Links.map((link) => (
+            <li
+              key={link.link}
+              className="hover:text-blue-400 duration-500 block md:inline-block mt-2 md:mt-2 mr-10"
+            >
+              <Link to={link.link}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 };
 
 export default Header;
+
+{
+  /* <nav className="top-0 sticky z-10 p-1 border-b border-black w-full  bg-white md:flex md:items-center md:justify-between">
+<div className="flex justify-between items-center">
+  <div className="flex item-center top-6 gap-2">
+    <Link to="/">
+      <img
+        src="logo.png"
+        alt="caffe-academy-logo"
+        className="h-auto w-40 py-6 cursor-pointer "
+      />
+    </Link>
+  </div>
+  <span className="flex items-center">
+    <span className="w-10 h-10 cursor-pointer md:hidden block">
+      <Link to="/login">
+        <UserCircleIcon />
+      </Link>
+    </span>
+    <span
+      onClick={() => setIsOpen((oldIsOpen) => !oldIsOpen)}
+      className="w-10 h-10 cursor-pointer md:hidden block"
+    >
+      {isOpen ? <XMarkIcon /> : <Bars3Icon />}
+    </span>
+  </span>
+</div>
+<ul
+  className={`md:flex md:items-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-300
+  ${isOpen ? "top-[80px] opacity-100 z-50 bg-white" : ""}`}
+>
+  {Links.map((link) => (
+    <li
+      key={link.link}
+      className="hover:text-blue-400 duration-500 mx-4 my-6 md:my-0"
+    >
+      <Link to={link.link}>{link.name}</Link>
+    </li>
+  ))}
+</ul>
+</nav> */
+}
