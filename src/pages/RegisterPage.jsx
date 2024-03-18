4386db2b6ca5906a83317e9f7389069be7f04b69
AleksaSmiljic <aleksa.smiljic11@gmail.com>
2024-03-18 17:38:10 +0200

    Added regex to RegisterPage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -3,28 +3,87 @@ import { InformationCircleIcon } from "@heroicons/react/24/solid";
 
 export function RegisterPage() {
   const [registerForm, setRegisterForm] = useState({
+    fullName: "",
     email: "",
     password: "",
+    passwordAgain: "",
   });
+
+  // const patterns = {
+  //   fullName: /^([a-zA-Z]{2,})\s([a-zA-Z]{2,})$/,
+  //   email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})$/,
+  //   password: /^[\w!@#$%^&*]{8,20}$/i,
+  // };
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setRegisterForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
   return (
     <div className="h-screen w-full flex items-center justify-center bg-red-200">
       <div>
         <InformationCircleIcon className="w-10 h-10" />
         <form>
-          <label htmlFor="email">
+          <label>
+            Full Name
+            <input
+              pattern="([a-zA-Z]{2,})\s([a-zA-Z]{2,})"
+              required
+              value={registerForm.fullName}
+              type="text"
+              className="block border "
+              name="fullName"
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
             Email
-            <input type="email" className="block border " />
+            <input
+              pattern="([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})"
+              required
+              value={registerForm.email}
+              type="email"
+              className="block border "
+              name="email"
+              onChange={handleInputChange}
+            />
           </label>
-          <label htmlFor="password">
+          <label>
             Password
-            <input type="password" className="block border " />
+            <input
+              pattern="[\w!@#$%^&*]{8,20}$"
+              required
+              value={registerForm.password}
+              type="password"
+              className="block border "
+              name="password"
+              onChange={handleInputChange}
+            />
           </label>
-          <label htmlFor="password">
+          <label>
             Password again
-            <input type="password" className="block border " />
+            <input
+              required
+              value={registerForm.passwordAgain}
+              type="password"
+              className="block border "
+              name="passwordAgain"
+              onChange={handleInputChange}
+            />
           </label>
+          <button className="block py-2 px-4 m-2 rounded-md border bg-blue-400">
+            REGISTER
+          </button>
         </form>
       </div>
     </div>
   );
 }
+
+// Uspešno ste se registrovali!
+
+// Čestitamo! Uspešno ste se registrovali. Sada možete uživati u brzom i jednostavnom naručivanju omiljene kafe online!
