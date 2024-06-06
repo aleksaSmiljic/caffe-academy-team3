77484f76385c1c4e3e4817ad167443e365774015
AleksaSmiljic <aleksa.smiljic11@gmail.com>
2024-06-06 20:48:01 +0200

    changed styles on Register Page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -40,26 +40,29 @@ export function RegisterPage() {
 
   return (
     <div className="flex flex-col bg-white items-center justify-center w-full h-screen">
+      <img
+        src="logo.png"
+        alt="caffee academy logo"
+        className="w-48 h-auto pb-28"
+      />
       {login ? (
         <div className="flex justify-center items-center flex-col">
-          <h2 className="text-3xl font-montserrat text-center">
+          <h2 className="text-3xl font-montserrat text-center font-medium">
             Uspešno ste se registrovali!
           </h2>
           <h2 className="text-lg m-6 text-center font-montserrat">
-            Čestitamo! Uspešno ste se registrovali. Sada možete uživati u brzom
-            i jednostavnom naručivanju omiljene kafe online!
+            <span className="text-[#248CC5]">Čestitamo!</span> Uspešno ste se
+            registrovali. Sada možete uživati u brzom i jednostavnom naručivanju
+            omiljene kafe online!
           </h2>
           <button className="block py-2 px-4 my-10 w-[200px] text-xl md:text-2xl text-white bg-[#248CC5] hover:bg-[#164864] duration-300 rounded-md font-montserrat">
             <Link to="/">Home Page</Link>
           </button>
         </div>
       ) : (
-        <>
-          <div className="w-full pb-20 flex justify-center items-center flex-col">
-            <InformationCircleIcon className="w-10 h-10 text-[#248CC5]" />
-            <p className="font-montserrat text-[#248CC5] w-[300px] text-center">
-              Da bi zavrišili porudžbinu, potrebno je da budete registrovani.
-            </p>
+        <div className="flex justify-center items-center flex-col">
+          <div className="text-2xl font-montserrat font-semibold text-[#164864]">
+            Registruj se
           </div>
           <div className="w-full">
             <form
@@ -117,17 +120,23 @@ export function RegisterPage() {
                   onChange={handleInputChange}
                 />
               </label>
-              <p className="underline font-montserrat text-sm text-[#248CC5]">
-                <Link to="/login">Već imate kreiran nalog? Prijavite se</Link>
-              </p>
-              <div className="w-full px-4 md:w-[400px]">
+
+              <div className="w-full px-4 md:w-[400px] flex items-center flex-col">
                 <button className="block py-2 px-4 my-10 w-full text-xl md:text-2xl text-white bg-[#248CC5] hover:bg-[#164864] duration-300 rounded-md font-montserrat">
                   Registruj se
                 </button>
+                <p className="underline font-montserrat text-sm">
+                  <Link to="/login">
+                    Već imate kreiran nalog?{" "}
+                    <span className="text-[#164864] font-semibold">
+                      Prijavite se
+                    </span>
+                  </Link>
+                </p>
               </div>
             </form>
           </div>
-        </>
+        </div>
       )}
     </div>
   );
